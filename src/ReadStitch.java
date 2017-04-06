2b218e048b52e459f2711ce4077e8f6cf894211a
remim-j <Remi.Marie-Jeanne@telecomnancy.eu>
2017-04-06 09:29:53 +0200

    Fonctional ReadStitch

--- src/ReadStitch.java
+++ src/ReadStitch.java
@@ -7,11 +7,12 @@ import java.io.InputStream;
 import java.util.Arrays;
 import java.util.Scanner;
 
-/** Reader for the tab separated values format (a basic table format without escapings or anything where the rows are separated by tabulators).**/
+	/**Takes args "CIDxxxxxxxxx" return a String ATC code like "XXXXXXXX"*/
+
 public class ReadStitch implements Closeable{
 
     final Scanner in;
-    String peekLine = null;
+    String line = null;
     File file = new File("C:/Users/user/Desktop/2A/GMD/projet/projet_2016-17/chemical.sources.v5.0.tsv");
     InputStream stream = new FileInputStream(file);
 
@@ -23,11 +24,9 @@ public class ReadStitch implements Closeable{
 
     /**Constructs a new TSVReader which produces values scanned from the specified input stream.*/
 
-    public ReadStitch(File f) throws FileNotFoundException {in = new Scanner(f);}
-
     public boolean hasNextTokens(){
 
-        if(peekLine!=null){
+        if(line!=null){
         	return true;
         }
         if(!in.hasNextLine()){
@@ -37,20 +36,56 @@ public class ReadStitch implements Closeable{
         if(line.isEmpty()) {
         	return hasNextTokens();
         }
-        this.peekLine = line;
+        this.line = line;
         return true;
     }
+
+    /** Return the wanted ATC ID*/
+
+    public String stitchCompoundIDToATCID(String stitchCompoundID){
+    	String[] token = null;
+    	while(hasNextTokens()){
+    		while(token == null){
+    			token = nextTokens();
+    		}
+    		if(token.length == 4 && token[2].equals("ATC")){
+    			System.out.println(token[1]);
+    			if(token[0].equals(transformChemical(stitchCompoundID)) || token[1].equals(transformAlias(stitchCompoundID))){
+    				return token[3];
+    			}
+    		}
+    		token = nextTokens();
+    	}
+    	System.out.println("No StitchCompoundID does match");
+    	return null;
+    }
+
+    /** Jump to the next line*/
 
     public String[] nextTokens(){
 
         if(!hasNextTokens()){
         	return null;
         }
-        String[] tokens = peekLine.split("[\\s\t]+");
-        System.out.println(Arrays.toString(tokens));
-        peekLine=null;
+        String[] tokens = line.split("[\\s\t]+");
+        line=null;
         return tokens;
     }
 
-    @Override public void close() throws IOException {in.close();}
+    public String transformChemical(String CID){
+    	String chemical = "";
+    	chemical = CID.substring(0, 3) + "m" + CID.substring(3, CID.length());
+    	return chemical;
+    }
+
+    public String transformAlias(String CID){
+    	String alias = "";
+    	alias = CID.substring(0, 3) + "s" + CID.substring(3, CID.length());
+    	return alias;
+    }
+
+    @Override public void close() throws IOException {
+    	in.close();
+    	}
+
 }
--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,21 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Test {
 
-	public static void main(String[] args){
+	public static void main(String[] args) throws IOException{
 
+		String ATCID;
 		ReadOmimOnto.CUIToPreferredLabel("C3549655");
-
+		ReadStitch TSV = new ReadStitch();
+		ATCID = TSV.stitchCompoundIDToATCID("CID00003393");
+		System.out.println(ATCID);
+		try {
+			TSV.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-
 }
