65eb0cb5ee09ed6cfd3488a8294f588a90f6c743
AuroreHusson <aurore.husson@telecomnancy.net>
2017-04-13 16:23:35 +0200

    Création de l'index pour Omim

---
+++ src/ReadOminIndex.java
@@ -0,0 +1,110 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.StoredField;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig.OpenMode;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+public class ReadOminIndex {
+  
+	private ReadOminIndex() {
+		String indexPath = "/home/aurore/workspace/GMD/index/Omim";
+		String docsPath = "omim_file";
+	    final Path docDir = Paths.get(docsPath);
+
+	    try {
+	    	System.out.println("Indexing Omim to directory '" + indexPath + "'..."); // to delete later
+	    	Directory dir = FSDirectory.open(Paths.get(indexPath));
+	    	Analyzer analyzer = new StandardAnalyzer();
+	    	IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
+	    	iwc.setOpenMode(OpenMode.CREATE_OR_APPEND);
+	    	IndexWriter writer = new IndexWriter(dir, iwc);
+	    	indexDoc(writer, docDir, Files.getLastModifiedTime(docDir).toMillis());
+	    	writer.close();
+	    	
+	    } catch (IOException e) {
+	      System.out.println(" caught a " + e.getClass() +
+	       "\n with message: " + e.getMessage());
+	    }
+	}
+	  
+	static void indexDoc(IndexWriter writer, Path file, long lastModified) throws IOException {
+		try {
+			InputStream ips = Files.newInputStream(file);
+			InputStreamReader ipsr = new InputStreamReader(ips);
+			BufferedReader br = new BufferedReader(ipsr);
+	    	
+			// make a new, empty document
+			Document doc = null;
+			String line = "", no = "";
+			boolean first = true;
+			int n = 0;
+			
+			while ((line=br.readLine()) != null) {
+	 
+				// initialize and save
+				if (line.startsWith("*RECORD*")) {
+					if (first) {
+						first = false;
+					} else {
+						//System.out.println("adding " + file); // to delete later
+						writer.addDocument(doc);
+					}
+					doc = new Document();
+				}
+				
+				// stocker TI
+				if (line.startsWith("*FIELD* TI")) {
+					n = no.length();
+					line = br.readLine().substring(n+2);
+					doc.add(new StoredField("TI", line));
+					System.out.println("TI "+ line); // to delete later
+					
+					while (!((line = br.readLine()).startsWith("*FIELD*"))) {
+						doc.add(new StoredField("TI", line));
+						System.out.println("TI "+ line); // to delete later
+					}
+				}
+				
+				// indexer CS
+				if (line.startsWith("*FIELD* CS")) {
+					while (!((line = br.readLine()).startsWith("*FIELD*"))) {
+						doc.add(new TextField("CS", line, Field.Store.NO));
+						System.out.println("CS "+ line); // to delete later
+					}
+				}
+				
+				// indexer NO
+				if (line.startsWith("*FIELD* NO")) {
+					no = br.readLine();
+					doc.add(new TextField("NO", no, Field.Store.NO));
+					System.out.println("NO "+ no); // to delete later
+				}  
+	      }
+
+		} catch (Exception e){
+	    	System.out.println(e.toString());
+	    }
+	}
+
+	public static void main(String[] args) {
+		long startTime = System.nanoTime();
+		new ReadOminIndex(); 
+		long endTime = System.nanoTime();
+		long duration = (endTime - startTime);
+		System.out.println("\nTime needed for indexing Omim : "+duration/Math.pow(10,9));
+	}
+}
